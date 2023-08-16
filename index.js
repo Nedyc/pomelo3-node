0f89dfe741ffb52481adb11ac84f6b39c4db9773
Andrea Dorigo <dorigo.and@gmail.com>
2023-08-16 14:22:29 +0200

    Adding the server side code

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const port = 3001;
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+io.on("connection", (client) => {
+  console.log("A user has connected");
+
+  client.on("disconnect", () => {
+    console.log("A user has disconnected");
+
+    const surveyId = client.surveyId;
+    client.broadcast
+      .to(surveyId)
+      .emit("message", { type: "userDisconnected", user: { id: client.id } });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
